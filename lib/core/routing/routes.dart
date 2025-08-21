900e71863be5d3eb036380f22e1917b0b365bfca
RawanMohamed521220 <rwanmohmed60@gmail.com>
2025-08-21 22:57:06 +0300

    Update routes.dart

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -3,4 +3,9 @@ class Routes {
   static const String registerScreen = "/register_screen";
   static const String profileScreen = "/profile_screen";
   static const String homeScreen = "/home_screen";
+  static const String onboarding1 = "/onboarding1";
+  static const String onboarding2 = "/onboarding2";
+  static const String onboarding3 = "/onboarding3";
+  
+  
 }
